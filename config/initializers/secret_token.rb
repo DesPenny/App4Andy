55a47791332840adaac8ec079199b76cd7147613
Des Penny <despenny@gmail.com>
2013-09-03 15:57:33 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App4Andy::Application.config.secret_token = '0c16c4c314a6a0e06655fb3fbbce397127d3302b7fbeca0bb6e66302c884797f988b4fbe3fa314d09cb6ad31e8bcfb7cbe334f03585682bc769abd23434b2725'
